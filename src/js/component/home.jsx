7d8842750467815ff4def2faced0a960cd104b92
BrodyDubach <117411713+BrodyDubach@users.noreply.github.com>
2023-11-07 01:29:13 +0000

    almost done with extra credit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [home, setHome] = useState("green");
+  const [intervalOn, setIntervalOn] = useState(true);
+  const [lights, setLights] = useState([]);
+  const [purpleLightOn, setPurpleLightOn] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (intervalOn) {
+      intervalId = setInterval(() => {
+        switch (home) {
+          case "red":
+            setHome("yellow");
+            break;
+          case "yellow":
+            setHome("green");
+            break;
+          case "green":
+            setHome("red");
+            break;
+
+        }
+
+        if (purpleLightOn) {
+          setLights((prevLights) =>
+            prevLights.length > 0 && prevLights[prevLights.length - 1] === "purple"
+              ? prevLights.slice(0, -1)
+              : [...prevLights, "purple"]
+          );
+        }
+      }, 2000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [home, intervalOn, purpleLightOn]);
+
+  const toggleInterval = () => {
+    setIntervalOn(!intervalOn);
+  };
+
+  const togglePurpleLight = () => {
+    setPurpleLightOn(!purpleLightOn);
+  };
+
+  return (
+    <>
+      <div className="stick mx-auto"></div>
+      <div className="container">
+        {lights.map((light, index) => (
+          <div key={index} className={`${light} light`}></div>
+        ))}
+        <div
+          className={home === "red" ? "red light glow" : "red light"}
+          onClick={() => setHome("red")}
+        ></div>
+        <div
+          className={home === "yellow" ? "yellow light glow" : "yellow light"}
+          onClick={() => setHome("yellow")}
+        ></div>
+        <div
+          className={home === "green" ? "green light glow" : "green light"}
+          onClick={() => setHome("green")}
+        ></div>
+
+        {purpleLightOn &&        
+		<div className={home === "purple" ? "purple light glow" : "purple light"}
+          onClick={() => setHome("purple")}
+        ></div>}
+      </div>
+      <button onClick={toggleInterval}>
+        {intervalOn ? "Stop Interval" : "Start Interval"}
+      </button>
+      <button onClick={togglePurpleLight}>
+        {purpleLightOn ? "Remove Purple Light" : "Add Purple Light"}
+      </button>
+    </>
+  );
 };
 
 export default Home;
